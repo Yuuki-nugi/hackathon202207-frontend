ec85354c576424e31b659568409345f599385bf5
Yuuki Kunugi <kunuyuu3@gmail.com>
2022-07-17 12:51:12 +0900

    sign inをちょっといい感じに

--- front/src/components/SignIn.jsx
+++ front/src/components/SignIn.jsx
@@ -3,6 +3,7 @@ import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { signIn } from "../api/auth";
 import { AuthContext } from "../App";
+import styled from "@emotion/styled";
 
 export const SignIn = () => {
   const { setIsSignedIn, setCurrentUser } = useContext(AuthContext);
@@ -40,34 +41,109 @@ export const SignIn = () => {
     }
   };
   return (
-    <>
-      <p>サインインページです。</p>
-      <form>
-        <div>
-          <label htmlFor="email">メールアドレス</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+    <div >
+      <SignInWrapper>
+        <LoginHeader>LOG IN</LoginHeader>
+        <LoginForm>
+          <div>
+            <LoginInput
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)
+              }
+              placeholder="email"
+            />
+          </div>
+          <div>
+            <LoginInput
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="password"
+            />
+          </div>
+          <SubmitBt type="submit" onClick={(e) => handleSignInSubmit(e)}>
+            Submit
+          </SubmitBt>
+        </LoginForm>
+        <div style={{backgroundColor: "white", width: "100%"}}>
+          <Link to="/signup" >sign up</Link>
         </div>
-        <div>
-          <label htmlFor="password">パスワード</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit" onClick={(e) => handleSignInSubmit(e)}>
-          Submit
-        </button>
-      </form>
-      <Link to="/signup">サインアップへ</Link>
-    </>
+      </SignInWrapper>
+    </div>
   );
 };
+
+const SignInWrapper = styled.div`
+  background: #456;
+  font-family: 'Open Sans', sans-serif;
+  width: 400px;
+  margin: 16px auto;
+  font-size: 16px;
+`
+
+const LoginHeader = styled.h2`
+  margin-top: 0;
+  margin-bottom: 0;
+  background: #28d;
+  padding: 20px;
+  font-size: 1.4em;
+  font-weight: normal;
+  text-align: center;
+  text-transform: uppercase;
+  color: #fff;
+`
+
+const LoginForm = styled.form`
+  background: #ebebeb;
+  padding: 12px;
+  padding: 20px;
+  font-size: 1.4em;
+  font-weight: normal;
+  text-align: center;
+  text-transform: uppercase;
+  color: #000000;
+`
+
+const LoginInput = styled.input`
+  box-sizing: border-box;
+  display: block;
+  width: 100%;
+  border-width: 1px;
+  border-style: solid;
+  padding: 16px;
+  outline: 0;
+  font-family: inherit;
+  font-size: 0.95em;
+  background: #fff;
+  border-color: #bbb;
+  color: #555;
+  margin-top: 16px;
+`
+
+const SubmitBt = styled.button`
+  box-sizing: border-box;
+  display: block;
+  width: 100%;
+  border-width: 1px;
+  border-style: solid;
+  padding: 16px;
+  outline: 0;
+  font-family: inherit;
+  font-size: 0.95em;
+  background: #28d;
+  border-color: transparent;
+  color: #fff;
+  margin-top: 16px;
+  cursor: pointer;
+  :hover {
+    background: #17c;
+  }
+  :focus {
+    border-color: #05a;
+  }
+`
